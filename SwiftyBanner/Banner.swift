ad9847aa1e8e8b4529144eb41f790e881271f7a0
Joseph Lin <josephlin0922@gmail.com>
2016-08-10 09:50:05 -0400

    wip

--- SwiftyBanner/Banner.swift
+++ SwiftyBanner/Banner.swift
@@ -16,6 +16,18 @@ public class WIBannerController : UIViewController {
         case Detail
     }
     
+    enum Style {
+        case BannerOnly
+        case BannerWithButton
+        case BannerWithDetail
+        case BannerWithDetailAndButton
+    }
+    
+    public typealias ButtonAction = (WIBannerController) -> Void
+    public typealias LabelConfig = (UILabel) -> Void
+    public typealias ImageViewConfig = (UIImageView) -> Void
+    public typealias ButtonConfig = (button: (UIButton) -> Void, action: ButtonAction)
+    
     //MARK: - Private Properties
 
     @IBOutlet private weak var bannerView: UIView?
@@ -29,6 +41,11 @@ public class WIBannerController : UIViewController {
     private weak var leftConstraint: NSLayoutConstraint?
     private weak var rightConstraint: NSLayoutConstraint?
     private weak var bottomConstraint: NSLayoutConstraint?
+
+    private var iconConfig: ImageViewConfig?
+    private var titleConfig: LabelConfig?
+    private var descriptionConfig: LabelConfig?
+    private var buttonConfig: ButtonConfig?
 
     //MARK: Private Convenience Properties
     
@@ -47,38 +64,74 @@ public class WIBannerController : UIViewController {
     }
     
     private var totalHeight: CGFloat {
-        return CGRectGetHeight(self.view.frame)
+        return self.detailHeight + self.bannerHeight
+    }
+
+    private var style: Style {
+        if descriptionConfig != nil && buttonConfig != nil {
+            return .BannerWithDetailAndButton
+        }
+        else if descriptionConfig != nil {
+            return .BannerWithDetail
+        }
+        else if buttonConfig != nil {
+            return .BannerWithButton
+        }
+        else {
+            return .BannerOnly
+        }
     }
     
     //MARK: - Public Properties
 
     private(set) public var state: State?
     
-    public var bannerIcon: UIImage? {
-        didSet {
-            self.bannerImageView?.image = self.bannerIcon
-        }
-    }
-    public var bannerTitle: String? {
-        didSet {
-            self.bannerLabel?.text = self.bannerTitle
-        }
-    }
-    public var detailDescription: String? {
-        didSet {
-            self.detailLabel?.text = self.detailDescription
-        }
-    }
-    public var detailButtonTitle: String? {
-        didSet {
-            self.detailButton?.setTitle(self.detailButtonTitle, forState: .Normal)
-        }
-    }
-    public var detailButtonAction: ((banner: WIBannerController) -> Void)?
-    
     //MARK: - Private Methods
 
+    @IBAction private func bannerTapped(sender: UITapGestureRecognizer) {
+        switch self.style {
+        case .BannerOnly:
+            break
+        case .BannerWithButton:
+            break
+        case .BannerWithDetail:
+            self.toggleDetail()
+        case .BannerWithDetailAndButton:
+            self.toggleDetail()
+        }
+    }
+    
     @IBAction private func bannerButtonTapped(sender: UIButton) {
+        switch self.style {
+        case .BannerOnly:
+            break
+        case .BannerWithButton:
+            if let buttonConfig = buttonConfig {
+                buttonConfig.action(self)
+            }
+        case .BannerWithDetail:
+            self.toggleDetail()
+        case .BannerWithDetailAndButton:
+            self.toggleDetail()
+        }
+    }
+    
+    @IBAction private func detailButtonTapped(sender: UIButton) {
+        switch self.style {
+        case .BannerOnly:
+            break
+        case .BannerWithButton:
+            break
+        case .BannerWithDetail:
+            break
+        case .BannerWithDetailAndButton:
+            if let buttonConfig = buttonConfig {
+                buttonConfig.action(self)
+            }
+        }
+    }
+    
+    private func toggleDetail() {
         guard let state = self.state else {
             return
         }
@@ -91,49 +144,83 @@ public class WIBannerController : UIViewController {
             self.setState(.Banner, animated: true)
         }
     }
-    
-    @IBAction private func detailButtonTapped(sender: UIButton) {
-        if let action = self.detailButtonAction {
-            action(banner: self)
+
+    private func updateUI() {
+        guard let bannerImageView = bannerImageView, bannerLabel = bannerLabel, bannerButton = bannerButton, detailLabel = detailLabel, detailButton = detailButton else {
+            // View hasn't been loaded yet.
+            return
         }
-    }
-    
-    func setupConstraintsAndAddToContainer(containerView: UIView) {
-
-        containerView.addSubview(self.view)
-
-        let left = NSLayoutConstraint(item: containerView, attribute: .Left, relatedBy: .Equal, toItem: self.view, attribute: .Left, multiplier: 1.0, constant: 0.0)
-        let right = NSLayoutConstraint(item: containerView, attribute: .Right, relatedBy: .Equal, toItem: self.view, attribute: .Right, multiplier: 1.0, constant: 0.0)
-        let bottom = NSLayoutConstraint(item: containerView, attribute: .Bottom, relatedBy: .Equal, toItem: self.view, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
-        containerView.addConstraints([left, right, bottom])
         
-        // Keep references to the constraints
-        self.leftConstraint = left
-        self.rightConstraint = right
-        self.bottomConstraint = bottom
+        if let iconConfig = iconConfig {
+            iconConfig(bannerImageView)
+        }
+
+        if let titleConfig = titleConfig {
+            titleConfig(bannerLabel)
+        }
+        
+        if let descriptionConfig = descriptionConfig {
+            descriptionConfig(detailLabel)
+        }
+
+        switch self.style {
+        case .BannerOnly:
+            bannerButton.setTitle(nil, forState: .Normal)
+            bannerButton.setImage(nil, forState: .Normal)
+            bannerButton.enabled = false
+            
+        case .BannerWithButton:
+            buttonConfig!.button(bannerButton)
+            bannerButton.enabled = true
+            
+        case .BannerWithDetail:
+            bannerButton.setTitle(nil, forState: .Normal)
+            if let state = state where state == .Detail {
+                bannerButton.setImage(UIImage(named: "ic_chevron_down"), forState: .Normal)
+            }
+            else {
+                bannerButton.setImage(UIImage(named: "ic_chevron_up"), forState: .Normal)
+            }
+            bannerButton.enabled = true
+            detailButton.setTitle(nil, forState: .Normal)
+            detailButton.setImage(nil, forState: .Normal)
+            detailButton.enabled = false
+            
+        case .BannerWithDetailAndButton:
+            bannerButton.setTitle(nil, forState: .Normal)
+            if let state = state where state == .Detail {
+                bannerButton.setImage(UIImage(named: "ic_chevron_down"), forState: .Normal)
+            }
+            else {
+                bannerButton.setImage(UIImage(named: "ic_chevron_up"), forState: .Normal)
+            }
+            bannerButton.enabled = true
+            buttonConfig!.button(detailButton)
+            detailButton.enabled = true
+        }
     }
     
     //MARK: - Public Methods
     
-    convenience init(title: String, icon: UIImage?, description: String?, buttonTitle: String?, buttonAction: ((WIBannerController) -> Void)?) {
+    convenience init() {
         self.init(nibName: "Banner", bundle: nil)
-        
-        self.bannerTitle = title
-        self.bannerIcon = icon
-        self.detailDescription = description
-        self.detailButtonTitle = buttonTitle
-        self.detailButtonAction = buttonAction
     }
     
     override public func viewDidLoad() {
         super.viewDidLoad()
         
-        self.bannerImageView?.image = self.bannerIcon
-        self.bannerLabel?.text = self.bannerTitle
-        self.detailLabel?.text = self.detailDescription
-        self.detailButton?.setTitle(self.detailButtonTitle, forState: .Normal)
+        self.updateUI()
     }
-
+    
+    public func configure(icon iconConfig: ImageViewConfig?, title titleConfig: LabelConfig?, description descriptionConfig: LabelConfig?, button buttonConfig: ButtonConfig?) {
+        self.iconConfig = iconConfig
+        self.titleConfig = titleConfig
+        self.descriptionConfig = descriptionConfig
+        self.buttonConfig = buttonConfig
+        
+        self.updateUI()
+    }
+    
     public func setState(state: State, animated: Bool, completion: (() -> Void)? = nil) {
         guard let superview = self.view.superview, bottomConstraint = self.bottomConstraint else {
             return
@@ -161,12 +248,14 @@ public class WIBannerController : UIViewController {
                     superview.layoutIfNeeded()
                 },
                 completion: { (finished) in
-                    self.bannerButton?.setImage(icon, forState: .Normal)
+                    self.updateUI()
+//                    self.bannerButton?.setImage(icon, forState: .Normal)
                 }
             )
         }
         else {
-            self.bannerButton?.setImage(icon, forState: .Normal)
+            self.updateUI()
+//            self.bannerButton?.setImage(icon, forState: .Normal)
         }
         
         self.state = state
@@ -174,6 +263,23 @@ public class WIBannerController : UIViewController {
 }
 
 //MARK: -
+
+extension WIBannerController {
+    func setupConstraintsAndAddToContainer(containerView: UIView) {
+        
+        containerView.addSubview(self.view)
+        
+        let left = NSLayoutConstraint(item: containerView, attribute: .Left, relatedBy: .Equal, toItem: self.view, attribute: .Left, multiplier: 1.0, constant: 0.0)
+        let right = NSLayoutConstraint(item: containerView, attribute: .Right, relatedBy: .Equal, toItem: self.view, attribute: .Right, multiplier: 1.0, constant: 0.0)
+        let bottom = NSLayoutConstraint(item: containerView, attribute: .Bottom, relatedBy: .Equal, toItem: self.view, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
+        containerView.addConstraints([left, right, bottom])
+        
+        // Keep references to the constraints
+        self.leftConstraint = left
+        self.rightConstraint = right
+        self.bottomConstraint = bottom
+    }
+}
 
 extension UIViewController {
     func presentBanner(banner: WIBannerController, animated: Bool, completion: (() -> Void)?) {
--- SwiftyBanner/ViewController.swift
+++ SwiftyBanner/ViewController.swift
@@ -10,17 +10,94 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func firstButtonTapped(sender: AnyObject) {
-        let banner = WIBannerController(title: "Test", icon: nil, description: "Test Test", buttonTitle: "Done") { banner in
-            banner.dismissBanner(animated: true, completion: nil)
-        }
+    @IBAction func bannerOnly(sender: AnyObject) {
+        let banner = WIBannerController()
+        banner.configure(
+            icon: { (imageView) in
+                imageView.image = UIImage(named: "ic_warning")
+                imageView.tintColor = UIColor.redColor()
+            },
+            title: { (label) in
+                label.text = "title"
+                label.textColor = UIColor.redColor()
+            },
+            description: nil,
+            button: nil
+        )
+        
         self.presentBanner(banner, animated: true, completion: nil)
     }
     
-    @IBAction func secondButtonTapped(sender: AnyObject) {
-        let banner = WIBannerController(title: "Test", icon: UIImage(named: "ic_warning"), description: "Test Test", buttonTitle: "Done") { banner in
-            banner.dismissBanner(animated: true, completion: nil)
-        }
-        self.presentBanner(banner, animated: false, completion: nil)
+    @IBAction func bannerWithButton(sender: AnyObject) {
+        let banner = WIBannerController()
+        banner.configure(
+            icon: { (imageView) in
+                imageView.image = UIImage(named: "ic_warning")
+                imageView.tintColor = UIColor.redColor()
+            },
+            title: { (label) in
+                label.text = "title"
+                label.textColor = UIColor.redColor()
+            },
+            description: nil,
+            button: (
+                button: { (button) in
+                    button.setImage(nil, forState: .Normal)
+                    button.setTitle("Done", forState: .Normal)
+                },
+                action: { banner in
+                    banner.dismissBanner(animated: true, completion: nil)
+                }
+            )
+        )
+        
+        self.presentBanner(banner, animated: true, completion: nil)
+    }
+    
+    @IBAction func bannerWithDetail(sender: AnyObject) {
+        let banner = WIBannerController()
+        banner.configure(
+            icon: { (imageView) in
+                imageView.image = UIImage(named: "ic_warning")
+                imageView.tintColor = UIColor.redColor()
+            },
+            title: { (label) in
+                label.text = "title"
+                label.textColor = UIColor.redColor()
+            },
+            description: { (label) in
+                label.text = "description"
+            },
+            button: nil
+        )
+        
+        self.presentBanner(banner, animated: true, completion: nil)
+    }
+    
+    @IBAction func bannerWithDetailAndButton(sender: AnyObject) {
+        let banner = WIBannerController()
+        banner.configure(
+            icon: { (imageView) in
+                imageView.image = UIImage(named: "ic_warning")
+                imageView.tintColor = UIColor.redColor()
+            },
+            title: { (label) in
+                label.text = "title"
+                label.textColor = UIColor.redColor()
+            },
+            description: { (label) in
+                label.text = "description"
+            },
+            button: (
+                button: { (button) in
+                    button.setTitle("Done", forState: .Normal)
+                },
+                action: { banner in
+                    banner.dismissBanner(animated: true, completion: nil)
+                }
+            )
+        )
+        
+        self.presentBanner(banner, animated: true, completion: nil)
     }
 }
